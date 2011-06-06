7f1d5664fb899105b73a5fee4e7127863996ae59
David Pollak <feeder.of.the.bears@gmail.com>
2011-06-06 09:07:17 -0700

    Updated to use Lift 2.4-M1 and to support Scala 2.9

---
+++ scala_29/lift_blank/project/build/LiftProject.scala
@@ -0,0 +1,20 @@
+import sbt._
+
+class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
+  val liftVersion = "2.4-M1"
+
+  // uncomment the following if you want to use the snapshot repo
+  //  val scalatoolsSnapshot = ScalaToolsSnapshots
+
+  // If you're using JRebel for Lift development, uncomment
+  // this line
+  // override def scanDirectories = Nil
+
+  override def libraryDependencies = Set(
+    "net.liftweb" %% "lift-webkit" % liftVersion % "compile",
+    "org.mortbay.jetty" % "jetty" % "6.1.22" % "test",
+    "junit" % "junit" % "4.5" % "test",
+    "ch.qos.logback" % "logback-classic" % "0.9.26",
+    "org.scala-tools.testing" %% "specs" % "1.6.8" % "test"
+  ) ++ super.libraryDependencies
+}
---
+++ scala_29/lift_mvc/sbt
@@ -0,0 +1,1 @@
+java -Xmx712M -Xss2M -XX:+CMSClassUnloadingEnabled -jar `dirname $0`/sbt-launcher.jar "$@"
