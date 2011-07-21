e41700567b7fca89aae42f52052a38228f8896bb
Indrajit Raychaudhuri <irc@indrajit.com>
2011-07-21 13:54:52 +0530

    Updated to use Lift 2.4-M3 along with updated dependencies

--- scala_28/lift_blank/project/build/LiftProject.scala
+++ scala_28/lift_blank/project/build/LiftProject.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
-  val liftVersion = "2.4-M2"
+  val liftVersion = property[Version].value.toString
 
   // uncomment the following if you want to use the snapshot repo
   //  val scalatoolsSnapshot = ScalaToolsSnapshots
@@ -12,8 +12,8 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
 
   override def libraryDependencies = Set(
     "net.liftweb" %% "lift-webkit" % liftVersion % "compile",
-    "org.mortbay.jetty" % "jetty" % "6.1.22" % "test",
-    "junit" % "junit" % "4.5" % "test",
+    "org.mortbay.jetty" % "jetty" % "6.1.26" % "test",
+    "junit" % "junit" % "4.7" % "test",
     "ch.qos.logback" % "logback-classic" % "0.9.26",
     "org.scala-tools.testing" %% "specs" % "1.6.6" % "test"
   ) ++ super.libraryDependencies
--- scala_28/lift_mvc/project/build/LiftProject.scala
+++ scala_28/lift_mvc/project/build/LiftProject.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
-  val liftVersion = "2.4-M2"
+  val liftVersion = property[Version].value.toString
 
   // uncomment the following if you want to use the snapshot repo
   //  val scalatoolsSnapshot = ScalaToolsSnapshots
@@ -13,10 +13,10 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
   override def libraryDependencies = Set(
     "net.liftweb" %% "lift-webkit" % liftVersion % "compile",
     "net.liftweb" %% "lift-mapper" % liftVersion % "compile",
-    "org.mortbay.jetty" % "jetty" % "6.1.22" % "test",
-    "junit" % "junit" % "4.5" % "test",
+    "org.mortbay.jetty" % "jetty" % "6.1.26" % "test",
+    "junit" % "junit" % "4.7" % "test",
     "ch.qos.logback" % "logback-classic" % "0.9.26",
     "org.scala-tools.testing" %% "specs" % "1.6.6" % "test",
-    "com.h2database" % "h2" % "1.2.138"
+    "com.h2database" % "h2" % "1.2.147"
   ) ++ super.libraryDependencies
 }
